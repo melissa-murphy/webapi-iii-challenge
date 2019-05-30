eb31afd2d9920c3b7731f8d2e869b166efc21292
Melissa Murphy <melissa@thoughtfulmonkey.design>
2019-05-29 22:17:29 -0700

    switch to npm, organizing userRouter

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,34 +1,37 @@
-const express = 'express';
+const express = require('express');
 
 const router = express.Router();
 
+const userDb = require('./userDb')
+const postDb = require('../posts/postDb')
+
 router.post('/', (req, res) => {
 
-});
+}); // Add User
 
 router.post('/:id/posts', (req, res) => {
 
-});
+}); //
 
 router.get('/', (req, res) => {
 
-});
+}); // Get Users
 
 router.get('/:id', (req, res) => {
 
-});
+}); // Get User by ID
 
 router.get('/:id/posts', (req, res) => {
 
-});
+}); // Get User Posts
 
 router.delete('/:id', (req, res) => {
 
-});
+}); // Delete User by ID
 
 router.put('/:id', (req, res) => {
 
-});
+}); // Update User
 
 //custom middleware
 
