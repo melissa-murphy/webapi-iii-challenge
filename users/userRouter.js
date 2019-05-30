b53913f8d2e756b18971b6de18fa39ce72b35c3a
melissa-murphy <melissamurphy.ca@gmail.com>
2019-05-30 08:21:00 -0700

    revert clean up

--- users/userRouter.js
+++ users/userRouter.js
@@ -2,54 +2,50 @@ const express = require('express');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+const userDb = require('./userDb');
+const postDb = require('../posts/postDb');
 
-});
+router.post('/', (req, res) => {}); // Add User
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', (req, res) => {}); //
 
-});
+router.get('/', (req, res) => {}); // Get Users
 
-router.get('/', (req, res) => {
+router.get('/:id', (req, res) => {}); // Get User by ID
 
-});
+router.get('/:id/posts', (req, res) => {}); // Get User Posts
 
-router.get('/:id', (req, res) => {
+router.delete('/:id', (req, res) => {}); // Delete User by ID
 
-});
-
-router.get('/:id/posts', (req, res) => {
-
-});
-
-router.delete('/:id', (req, res) => {
-
-});
-
-router.put('/:id', (req, res) => {
-
-});
+router.put('/:id', (req, res) => {}); // Update User
 
 //custom middleware
 
-function validateUserId(req, res, next) {
-    // if user id is is valid
+async function validateUserId(req, res, next) {
+  const userId = req.params.id;
+  const user = await userDb.getById(userId);
+  let condition = [userId !== null && userId !== '' && userId !== 0];
 
-    // return and store as req.user
+  if (userId) {
+    if (condition) {
+      user;
+      if (user) {
+        req.user = user;
+        next();
+      } else {
+        res.status(400).json({ message: `This user does not exist` });
+      }
+    } else {
+      res.status(400).json({ message: `Invalid ID` });
+    }
+  } else {
+    res.status(400).json({ message: `Please provide User ID` });
+  }
+}
 
-    // else return error 400 invalid
 
-};
+function validateUser(req, res, next) {}
 
-function validateUser(req, res, next) {
-
-
-};
-
-function validatePost(req, res, next) {
-
-};
+function validatePost(req, res, next) {}
 
 module.exports = router;
-
-
