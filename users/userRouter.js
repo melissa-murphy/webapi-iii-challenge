374a4e413ec7a5ea375e0b3d9501bea53ede4ab2
melissa-murphy <melissamurphy.ca@gmail.com>
2019-05-30 08:00:09 -0700

    Revert "validate user id. new style :D"
    This reverts commit a6e8aa3928de10ede194865d08c581c3d813b53f.

--- users/userRouter.js
+++ users/userRouter.js
@@ -2,50 +2,49 @@ const express = require('express');
 
 const router = express.Router();
 
-const userDb = require('./userDb');
-const postDb = require('../posts/postDb');
+const userDb = require('./userDb')
+const postDb = require('../posts/postDb')
 
-router.post('/', (req, res) => {}); // Add User
+router.post('/', (req, res) => {
 
-router.post('/:id/posts', (req, res) => {}); //
+}); // Add User
 
-router.get('/', (req, res) => {}); // Get Users
+router.post('/:id/posts', (req, res) => {
 
-router.get('/:id', (req, res) => {}); // Get User by ID
+}); //
 
-router.get('/:id/posts', (req, res) => {}); // Get User Posts
+router.get('/', (req, res) => {
 
-router.delete('/:id', (req, res) => {}); // Delete User by ID
+}); // Get Users
 
-router.put('/:id', (req, res) => {}); // Update User
+router.get('/:id', (req, res) => {
+
+}); // Get User by ID
+
+router.get('/:id/posts', (req, res) => {
+
+}); // Get User Posts
+
+router.delete('/:id', (req, res) => {
+
+}); // Delete User by ID
+
+router.put('/:id', (req, res) => {
+
+}); // Update User
 
 //custom middleware
 
-async function validateUserId(req, res, next) {
-  const userId = req.params.id;
-  const user = await userDb.getById(userId);
-  let condition = [userId !== null && userId !== '' && userId !== 0];
+function validateUserId(req, res, next) {
 
-  if (userId) {
-    if (condition) {
-      user;
-      if (user) {
-        req.user = user;
-        next();
-      } else {
-        res.status(400).json({ message: `This user does not exist` });
-      }
-    } else {
-      res.status(400).json({ message: `Invalid ID` });
-    }
-  } else {
-    res.status(400).json({ message: `Please provide User ID` });
-  }
-}
+};
 
+function validateUser(req, res, next) {
 
-function validateUser(req, res, next) {}
+};
 
-function validatePost(req, res, next) {}
+function validatePost(req, res, next) {
+
+};
 
 module.exports = router;
