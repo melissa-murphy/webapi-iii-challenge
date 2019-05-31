3644e3cfedb3067933c838cd99862248a5015ed7
Melissa Murphy <melissa@thoughtfulmonkey.design>
2019-05-31 06:31:07 -0700

    server changes

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const helmet = require('helmet');
 const userRouter = require('./users/userRouter');
 const postRouter = require('./posts/postRouter');
 
@@ -6,20 +7,25 @@ const server = express();
 
 // Global Middleware
 server.use(express.json());
+server.use(helmet);
 server.use(logger);
-server.use('/users/', userRouter);
-server.use('/posts/', postRouter);
-
 
 // Default GET
 server.get('/', (req, res) => {
   res.send(`<h2>Hello World from Eternal Screaming!</h2>`);
 });
 
+server.use('api/users', userRouter);
+server.use('api/posts', postRouter);
+
 //custom middleware
 
 function logger(req, res, next) {
-  console.log(`A ${req.method} request for '${req.url}', reqested: ${new Date().toUTCString()}`);
+  console.log(
+    `A ${req.method} request for '${
+      req.url
+    }', reqested: ${new Date().toUTCString()}`
+  );
   next();
 }
 
